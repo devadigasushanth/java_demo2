7e4c6ea5b3383066f858c873b35de3aec328da96
devadigasushanth <158567204+devadigasushanth@users.noreply.github.com>
2024-02-14 09:33:50 +0530

    Add files via upload

---
+++ prgmgcd.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class prgmgcd
+{
+	public static void main(String args[])
+	{
+		Scanner S=new Scanner(System.in);
+		System.out.println("enter the n value");
+		int m=S.nextInt();
+		System.out.println("enter the n value");
+		int n=S.nextInt();
+		while(m!=n)
+		{
+			if(m>n)
+			m=m-n;
+			else
+				n=n-m;
+		}
+	
+	System.out.println("GCD of given number="+m);
+	}
+}
